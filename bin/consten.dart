9a59d32031132c8e38c0503ee649abe159ffd4ba
abdullakanon2 <abdullakanon2@gmail.com>
2023-01-20 22:51:23 +0600

    first commit

---
+++ bin/consten.dart
@@ -0,0 +1,9 @@
+class Abdulllla {
+  var Love;
+  var fuck;
+
+  Abdulllla(var a, var b) {
+    Love = a;
+    b = fuck;
+  }
+}
---
+++ bin/monir.dart
@@ -0,0 +1,25 @@
+class Monir {
+  var a = "i love Rita";
+  var b = "I love Bangladesh";
+
+  Monir() {
+    int a = 23;
+    int b = 23;
+    print(a + b);
+  }
+  void min() {
+    var name = "Abdulla";
+    var age = "34";
+    print(name + age);
+  }
+
+  void nin() {
+    var name = "Abdulla kanon";
+    print(name);
+  }
+
+  void ain() {
+    var age = "45";
+    print(age);
+  }
+}
